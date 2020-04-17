6d08a3973cf3dc070b8abc638c0c4569bdbed30a
Kobar <kobar@alterra.id>
2020-04-18 03:32:41 +0700

    add twitter card component

---
+++ components/TwitterCard.js
@@ -0,0 +1,33 @@
+const TwitterCard = ({ content }) => {
+  return (
+    <div className="container">
+      <div className="card text-white bg-info mb-3">
+        <div className="card-body">
+          <div className="card-title">
+            <h2>
+              <span className="pr-3">
+                <img src="./svg/twitter.svg" width="26" height="26" />
+              </span>
+              Pemkot Malang
+            </h2>
+          </div>
+          <br />
+          <p className="card-text">{content}</p>
+        </div>
+        <style jsx>{`
+          .bg-info {
+            background-color: #26c6da;
+          }
+          .card {
+            width: 400px !important;
+            text-align: left;
+            box-shadow: -1px 15px 30px -12px black;
+            max-height: 400px;
+          }
+        `}</style>
+      </div>
+    </div>
+  )
+}
+
+export default TwitterCard
