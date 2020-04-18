413a19c71e156131efe7b97b69715267ae5f2af7
Kobar <kobar@alterra.id>
2020-04-18 16:04:49 +0700

    add meta for SEO

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,9 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <html>
+      <html lang="id">
         <Head>
+          <title>Covid App Kota Malang</title>
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css"
@@ -23,6 +24,10 @@ class MyDocument extends Document {
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
             crossOrigin="anonymous"
+          />
+          <meta
+            name="description"
+            content="Covid tracker kota Malang per kecamatan"
           />
         </Head>
         <body>
